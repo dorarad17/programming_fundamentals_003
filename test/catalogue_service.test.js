b81b726fb4e4edd0fb68797fb745477d857b9ab3
Dora <dorarad17@gmail.com>
2019-11-22 12:05:51 +0000

    Initial practice commit

--- test/catalogue_service.test.js
+++ test/catalogue_service.test.js
@@ -8,4 +8,36 @@ describe("catalogueService", () => {
       expect(catalogueService.countBooksByAuthor("Charles Dickens")).toBe(3);
     });
   });
-});
+
+  describe("catalogueService.checkBookByTitle", () => {
+    test("returns true if the book exists", () => {
+      expect(
+        catalogueService.checkBookByTitle(
+          "The Assassination of Margaret Thatcher"
+        )
+      ).toBe(true);
+    });
+  });
+    test("returns false if the book does not exists", () => {
+      expect(
+        catalogueService.checkBookByTitle(
+          "hdhdh"
+        )
+      ).toBe(false);
+    });
+  });
+
+    describe("catalogueService.countBooksByFirstLetter", () => {
+      test("returns the total number of books beginning with a given letter", () => {
+        expect(catalogueService.countBooksByFirstLetter("G")).toBe(2);
+      });
+    });
+
+    describe("catalogueService.getQuantity", () => {
+      test("returns the total number of a certain book in stock", () => {
+        expect(catalogueService.getQuantity("The Yellow Wallpaper")).toBe(12);
+      });
+    });
+
+
+    
